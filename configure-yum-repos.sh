d8c9799a9e8b10cc52533e7eaa053b69f820aebe
hberrady <127338251+hberrady@users.noreply.github.com>
2024-07-30 23:23:37 +0100

    Create configure-yum-repos.sh
    This repository contains a script for configuring yum repositories on CentOS 7. The configure-yum-repos.sh script updates and sets up the CentOS Base and EPEL repositories with specific configurations, ensuring that the system uses the correct URLs and settings for package management and updates.

---
+++ configure-yum-repos.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Copyright (C) 2024 HICHAM BERRADY
+# This script replaces the content of CentOS-Base.repo and epel.repo with specified configurations.
+
+# Replace content of /etc/yum.repos.d/CentOS-Base.repo
+sudo tee /etc/yum.repos.d/CentOS-Base.repo <<EOF
+[base]
+name=CentOS-\$releasever - Base
+baseurl=http://vault.centos.org/7.9.2009/os/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#released updates
+[updates]
+name=CentOS-\$releasever - Updates
+baseurl=http://vault.centos.org/7.9.2009/updates/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that may be useful
+[extras]
+name=CentOS-\$releasever - Extras
+baseurl=http://vault.centos.org/7.9.2009/extras/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that extend functionality of existing packages
+[centosplus]
+name=CentOS-\$releasever - Plus
+baseurl=http://vault.centos.org/7.9.2009/centosplus/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+# Replace content of /etc/yum.repos.d/epel.repo
+sudo tee /etc/yum.repos.d/epel.repo <<EOF
+[epel]
+name=Extra Packages for Enterprise Linux 7 - \$basearch
+metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+
+[epel-debuginfo]
+name=Extra Packages for Enterprise Linux 7 - \$basearch - Debug
+metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-debug-7&arch=\$basearch
+failovermethod=priority
+enabled=0
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+
+[epel-source]
+name=Extra Packages for Enterprise Linux 7 - \$basearch - Source
+metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-source-7&arch=\$basearch
+failovermethod=priority
+enabled=0
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+EOF
+
+# Clear yum cache
+sudo yum clean all
